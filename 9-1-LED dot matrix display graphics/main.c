179ec6202cf96bc098225a7c02cf84077f3b357e
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-11-16 22:29:37 +0800

    practice

--- 9-1-LED dot matrix display graphics/main.c
+++ 9-1-LED dot matrix display graphics/main.c
@@ -1,11 +1,17 @@
 #include "Delay.h"
 
-unsigned char Sec;
-
 void main()
 {
-	while(1)
+	MatrixLED_Init();
+	while (1)
 	{
-		;
+		MatrixLED_Show(0, 0x3C);
+		MatrixLED_Show(1, 0x42);
+		MatrixLED_Show(2, 0xA9);
+		MatrixLED_Show(3, 0x85);
+		MatrixLED_Show(4, 0x85);
+		MatrixLED_Show(5, 0xA9);
+		MatrixLED_Show(6, 0x42);
+		MatrixLED_Show(7, 0x3C);
 	}
-} 
+}
